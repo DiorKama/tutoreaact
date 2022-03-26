1c5bbfb05fcf8b4dae413bf8c12a695e679cc606
dior <diorkama@gmail.com>
2022-03-26 13:39:01 +0000

    tuto react

---
+++ src/pages/About.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum soluta excepturi ratione ea.
+            Hic error dignissimos omnis, vero modi neque, molestias at id maxime ipsam enim eos quibusdam vel!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum soluta excepturi ratione ea.
+            Hic error dignissimos omnis, vero modi neque, molestias at id maxime ipsam enim eos quibusdam vel!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum soluta excepturi ratione ea.
+            Hic error dignissimos omnis, vero modi neque, molestias at id maxime ipsam enim eos quibusdam vel!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum soluta excepturi ratione ea.
+            Hic error dignissimos omnis, vero modi neque, molestias at id maxime ipsam enim eos quibusdam vel!
+            </p>
+            <br />
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum soluta excepturi ratione ea.
+            Hic error dignissimos omnis, vero modi neque, molestias at id maxime ipsam enim eos quibusdam vel!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum soluta excepturi ratione ea.
+            Hic error dignissimos omnis, vero modi neque, molestias at id maxime ipsam enim eos quibusdam vel!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum soluta excepturi ratione ea.
+            Hic error dignissimos omnis, vero modi neque, molestias at id maxime ipsam enim eos quibusdam vel!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum soluta excepturi ratione ea.
+            Hic error dignissimos omnis, vero modi neque, molestias at id maxime ipsam enim eos quibusdam vel!
+            </p>
+        </div>
+    );
+};
+
+export default About;
